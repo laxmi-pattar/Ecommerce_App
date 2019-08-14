5ffaca438a6e807c90949d65c8f3b3bbde1842d4
Laxmi Pattar <laxmipattar710@gmail.com>
2019-08-14 19:36:03 +0530

    Ecommerce_App

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,9 @@
+class ProductsController < InheritedResources::Base
+
+  private
+
+    def product_params
+      params.require(:product).permit(:title, :description, :price, :published, :category_id)
+    end
+
+end
---
+++ db/seeds.rb
@@ -0,0 +1,22 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+#AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+#50.times do |i|
+	#Product.create(title: "Sample Title#{i}",
+	#	description: "Title Description#{i}",
+	#	price: 5.5 * 10,
+	#	published: 'false')
+#end
+
+10.times do |i|
+	Category.create(name: "Category #{i}", 
+		description: "Category #{i}")
+end
+
+
